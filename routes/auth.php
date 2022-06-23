36c24697b9513872f3fc283452425f4d2e21c161
Ezequiel Costa <ezequielteixeiracosta321@gmail.com>
2022-06-23 01:31:50 -0300

    Mudança do controller de login

--- routes/auth.php
+++ routes/auth.php
@@ -6,13 +6,18 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
                 ->middleware('guest')
                 ->name('register');
 
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+//Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+//                ->middleware('guest')
+//                ->name('login');
+
+Route::post('/login', [AuthController::class, 'login'])
                 ->middleware('guest')
                 ->name('login');
 
